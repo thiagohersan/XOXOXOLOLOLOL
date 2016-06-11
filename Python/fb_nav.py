f9d68256525f5ae7d1285b8cbfc4fc693f8cdc9d
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-06-11 13:11:01 -0300

    better variable names

--- Python/fb_nav.py
+++ Python/fb_nav.py
@@ -55,19 +55,19 @@ sleep(1)
 #mDriver.set_window_position(0,0)
 
 # get link to profile
-mAction = ActionChains(mDriver)
-pl = mDriver.find_element_by_xpath("//a[@data-testid='blue_bar_profile_link']")
-mAction.click(pl).perform()
+profileLink = mDriver.find_element_by_xpath("//a[@data-testid='blue_bar_profile_link']")
+profileHref = profileLink.get_attribute('href')
+ActionChains(mDriver).click(profileLink).perform()
 sleep(1)
 
 # get link to friends
-fl = mDriver.find_element_by_xpath("//a[@data-tab-key='friends']")
-fhref = fl.get_attribute('href')
-mDriver.execute_script("window.scrollTo(arguments[0], arguments[1]);", fl.location['x'], fl.location['y']-50)
-#mAction = ActionChains(mDriver).key_down(Keys.SHIFT).click(fl).key_up(Keys.SHIFT)
+friendLink = mDriver.find_element_by_xpath("//a[@data-tab-key='friends']")
+friendHref = friendLink.get_attribute('href')
+mDriver.execute_script("window.scrollTo(%s, %s);"%(friendLink.location['x'], friendLink.location['y']-50))
+
 
 for i in range(3):
-    ActionChains(mDriver).key_down(Keys.SHIFT).click(fl).key_up(Keys.SHIFT).perform()
+    ActionChains(mDriver).key_down(Keys.SHIFT).click(friendLink).key_up(Keys.SHIFT).perform()
 
     # huge hack to resize on creation. wtf. seriously?!?
     mDriver.switch_to_window(mDriver.window_handles[-1])
@@ -77,6 +77,7 @@ for i in range(3):
     sleep(0.5)
 sleep(1)
 
+# close original window
 mDriver.switch_to_window(mDriver.window_handles[0])
 mDriver.close()
 
@@ -87,8 +88,8 @@ def loadChoseAndScrollFriends():
         for w in mDriver.window_handles:
             if (uniform(0.0,1.0) > 0.5):
                 mDriver.switch_to_window(w)
-                mw = mDriver.execute_script("return document.body.scrollWidth;")
-                mDriver.execute_script("window.scrollTo(%s, document.body.scrollHeight);"%randint(mw/5,mw/2))
+                bodyWidth = mDriver.execute_script("return document.body.scrollWidth;")
+                mDriver.execute_script("window.scrollTo(%s, document.body.scrollHeight);"%randint(bodyWidth/5,bodyWidth/2))
                 sleep(0.1)
 
     # chose friends and go to profiles
@@ -96,10 +97,10 @@ def loadChoseAndScrollFriends():
         mDriver.switch_to_window(w)
         #mDriver.execute_script("window.scrollTo(0, 0);")
         sleep(0.5)
-        fls = mDriver.find_elements_by_xpath("//div[@class='fsl fwb fcb']")
-        fl = fls[randint(0,len(fls)-1)]
-        mDriver.execute_script("window.scrollTo(%s, %s);"%(fl.location['x'], fl.location['y']-100))
-        fl.click()
+        friendList = mDriver.find_elements_by_xpath("//div[@class='fsl fwb fcb']")
+        bffLink = friendList[randint(0,len(friendList)-1)]
+        mDriver.execute_script("window.scrollTo(%s, %s);"%(bffLink.location['x'], bffLink.location['y']-100))
+        bffLink.click()
         sleep(0.5)
 
     # scroll around a couple of times
@@ -107,15 +108,16 @@ def loadChoseAndScrollFriends():
         for w in mDriver.window_handles:
             if (uniform(0.0, 1.0) > 0.3):
                 mDriver.switch_to_window(w)
-                mw = mDriver.execute_script("return document.body.scrollWidth;")
-                mh = mDriver.execute_script("return document.body.scrollHeight;")
-                mDriver.execute_script("window.scrollTo(%s, %s);"%(randint(mw/8,mw/2), randint(0,mh)))
+                bodyWidth = mDriver.execute_script("return document.body.scrollWidth;")
+                bodyHeight = mDriver.execute_script("return document.body.scrollHeight;")
+                mDriver.execute_script("window.scrollTo(%s, %s);"%(randint(bodyWidth/8,bodyWidth/2), randint(0,bodyHeight)))
                 # everyone in a while like something
                 if(uniform(0.0, 1.0) > 0.7):
-                    lls = mDriver.find_elements_by_xpath("//a[@data-testid='fb-ufi-likelink']")
-                    if(len(lls) > 1):
-                        ll = lls[randint(0,len(lls)-1)]
-                        ActionChains(mDriver).move_to_element(ll).perform()
+                    likeLinks = mDriver.find_elements_by_xpath("//a[@data-testid='fb-ufi-likelink']")
+                    if(len(likeLinks) > 1):
+                        superLike = likeLinks[randint(0,len(likeLinks)-1)]
+                        ActionChains(mDriver).move_to_element(superLike).perform()
+                        # TODO: click ?!?
                         sleep(0.5)
                 sleep(0.1)
 
@@ -123,16 +125,15 @@ def backToFriends():
     # go back to list of friends and repeat
     for w in mDriver.window_handles:
         mDriver.switch_to_window(w)
-        mDriver.get(fhref)
+        mDriver.get(friendHref)
         sleep(0.1)
-        mw = mDriver.execute_script("return document.body.scrollWidth;")
-        mh = mDriver.execute_script("return document.body.scrollHeight;")
-        mDriver.execute_script("window.scrollTo(%s, %s);"%(randint(mw/8,mw/2), randint(0,mh)))
+        bodyWidth = mDriver.execute_script("return document.body.scrollWidth;")
+        bodyHeight = mDriver.execute_script("return document.body.scrollHeight;")
+        mDriver.execute_script("window.scrollTo(%s, %s);"%(randint(bodyWidth/8,bodyWidth/2), randint(0,bodyHeight)))
         sleep(0.1)
-
 
 
 for i in range(8):
     loadChoseAndScrollFriends()
     backToFriends()
-loadChoseAndScrollFriends()    
+
